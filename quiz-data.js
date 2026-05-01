3f85424ab29854ecc5610bebf458c42805bc4f73
ronaldolima534 <ronaldolimaymanta5@gmail.com>
2026-05-01 13:42:17 -0500

    Update quiz-data.js

--- quiz-data.js
+++ quiz-data.js
@@ -1,43 +1,46 @@
 const quizData = {
 
-  "transfers": {
+  transfers: {
     titleKey: "quiz_transfers_title",
-    categories: ["players", "transfers"],
     folder: "images/transfers",
     questionPrefix: "q",
     answerPrefix: "a",
+
     totalEasy: 20,
     totalMedium: 20,
     totalHard: 20,
-    thumbnail: "images/thumbnails/quiz1.jpg",
+
+    thumbnail: "images/thumbnails/transfers.jpg",
     showOnHome: true,
     homeOrder: 1
   },
 
   "player-clues": {
     titleKey: "quiz_player_clues_title",
-    categories: ["players"],
     folder: "images/player-clues",
     questionPrefix: "q",
     answerPrefix: "a",
+
     totalEasy: 20,
     totalMedium: 20,
     totalHard: 20,
-    thumbnail: "images/thumbnails/quiz2.jpg",
+
+    thumbnail: "images/thumbnails/player-clues.jpg",
     showOnHome: true,
     homeOrder: 2
   },
 
   "nationality-club": {
     titleKey: "quiz_nationality_club_title",
-    categories: ["clubs", "national-teams"],
     folder: "images/nationality-club",
     questionPrefix: "q",
     answerPrefix: "a",
-    totalEasy: 20,
-    totalMedium: 20,
-    totalHard: 20,
-    thumbnail: "images/thumbnails/quiz3.jpg",
+
+    totalEasy: 10,
+    totalMedium: 10,
+    totalHard: 10,
+
+    thumbnail: "images/thumbnails/nationality-club.jpg",
     showOnHome: true,
     homeOrder: 3
   }
